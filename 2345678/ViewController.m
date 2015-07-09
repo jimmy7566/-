700df1e26392ab828ba7b6304be2e06fe59749e6
lanou3g <linker784533@126.com>
2015-07-09 14:47:57 +0800

    no message

---
+++ 2345678/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  2345678
+//
+//  Created by lanou3g on 15/7/9.
+//  Copyright (c) 2015年 周文. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
